e96cb99126613eac98a77077ed0ca2b53a431e37
jvav-runtime-environment <dannybxx@qq.com>
2025-03-18 18:45:42 +0800

    优化代码格式

--- src/main/java/cn/JvavRE/playerTopList/ui/UI.java
+++ src/main/java/cn/JvavRE/playerTopList/ui/UI.java
@@ -41,15 +41,16 @@ public class UI {
             tempList.add(UIConfig.get(UIComponent.ITEM)
                     .replaceText(config -> config.match(Config.getPattern())
                             .replacement((matchResult, builder) -> {
-                                        String key = matchResult.group();
-                                        return switch (key) {
-                                            case "{num}" -> Component.text(String.valueOf(num));
-                                            case "{playerName}" -> Component.text(playerName);
-                                            case "{count}" -> Component.text(String.valueOf(playerData.getCount()));
-                                            default -> Component.text(key);
-                                        };
-                                    }
-                            ))
+                                String key = matchResult.group();
+                                return switch (key) {
+                                    case "{num}" -> Component.text(String.valueOf(num));
+                                    case "{playerName}" -> Component.text(playerName);
+                                    case "{count}" -> Component.text(String.valueOf(playerData.getCount()));
+                                    default -> Component.text(key);
+                                };
+
+                            })
+                    )
             );
         }
     }
@@ -58,31 +59,30 @@ public class UI {
         int totalPage = PageMgr.getTotalPages(tempList);
 
         Bukkit.getAsyncScheduler().runNow(PlayerTopList.getInstance(), task -> {
-                    Component nextButton = UIConfig.get(UIComponent.NEXT_BUTTON)
-                            .clickEvent(ClickEvent.runCommand("/ptl show " + topList.getName() + " " + (page + 1)));
-                    Component prevButton = UIConfig.get(UIComponent.PREV_BUTTON)
-                            .clickEvent(ClickEvent.runCommand("/ptl show " + topList.getName() + " " + (page - 1)));
+            Component nextButton = UIConfig.get(UIComponent.NEXT_BUTTON)
+                    .clickEvent(ClickEvent.runCommand("/ptl show " + topList.getName() + " " + (page + 1)));
+            Component prevButton = UIConfig.get(UIComponent.PREV_BUTTON)
+                    .clickEvent(ClickEvent.runCommand("/ptl show " + topList.getName() + " " + (page - 1)));
 
-                    player.sendMessage(UIConfig.get(UIComponent.MAIN_UI)
-                            .replaceText(config -> config.match(Config.getPattern())
-                                    .replacement((matchResult, builder) -> {
-                                                String key = matchResult.group();
-                                                return switch (key) {
-                                                    case "{items}" -> PageMgr.getPage(tempList, page);
-                                                    case "{listName}" -> coloredName;
-                                                    case "{updateTime}" -> Component.text(topList.getUpdateTime());
-                                                    case "{totalIndex}" -> Component.text(String.valueOf(totalPage));
-                                                    case "{currentIndex}" -> Component.text(String.valueOf(page));
-                                                    case "{prevButton}" -> page > 1 ? prevButton : Component.empty();
-                                                    case "{nextButton}" -> page < totalPage ? nextButton : Component.empty();
-                                                    default -> Component.text(key);
-                                                };
-                                            }
-                                    )
-                            )
-                    );
-                }
-        );
+            player.sendMessage(UIConfig.get(UIComponent.MAIN_UI)
+                    .replaceText(config -> config.match(Config.getPattern())
+                            .replacement((matchResult, builder) -> {
+                                String key = matchResult.group();
+                                return switch (key) {
+                                    case "{items}" -> PageMgr.getPage(tempList, page);
+                                    case "{listName}" -> coloredName;
+                                    case "{updateTime}" -> Component.text(topList.getUpdateTime());
+                                    case "{totalIndex}" -> Component.text(String.valueOf(totalPage));
+                                    case "{currentIndex}" -> Component.text(String.valueOf(page));
+                                    case "{prevButton}" -> page > 1 ? prevButton : Component.empty();
+                                    case "{nextButton}" -> page < totalPage ? nextButton : Component.empty();
+                                    default -> Component.text(key);
+                                };
+
+                            })
+                    )
+            );
+        });
     }
 
     public Component getColoredName() {

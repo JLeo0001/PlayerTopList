2080ce1502d4f257ba1788f3033ceb8b5ac092ff
jvav-runtime-environment <dannybxx@qq.com>
2025-03-27 19:30:39 +0800

    修正了UI按钮无法配置的问题

--- src/main/java/cn/JvavRE/playerTopList/config/UIComponent.java
+++ src/main/java/cn/JvavRE/playerTopList/config/UIComponent.java
@@ -6,7 +6,7 @@ import net.kyori.adventure.text.minimessage.MiniMessage;
 public enum UIComponent {
     HEADER("header", "<aqua>================[ <hover:show_text:'<yellow>更新时间: <#FFA500>{updateTime}</#FFA500></yellow>'>{listName} ]================</aqua><newline>"),
     FOOTER("footer", "<aqua>{prevButton}当前: (<white>{currentIndex}</white>/<white>{totalIndex}</white>){nextButton}</aqua>"),
-    PREV_BUTTON("previous-button", "<hover:show_text:'上一页'><dark_aqua> <- </dark_aqua></hover>"),
+    PREV_BUTTON("prev-button", "<hover:show_text:'上一页'><dark_aqua> <- </dark_aqua></hover>"),
     NEXT_BUTTON("next-button", "<hover:show_text:'下一页'><dark_aqua> -> </dark_aqua></hover>"),
     ITEM("item", "<gray>⬡</gray> <white>{num}. <green>{playerName} <grey>{spacer}</grey> {count}</green></white>"),
     MAIN_UI("you-should-not-use-this", "this-will-generate-on-default"),

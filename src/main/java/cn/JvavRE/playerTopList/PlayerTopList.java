fd9e34df3daac4b20cc7dfcbc339820617542c79
JLeo0001 <104238511+JLeo0001@users.noreply.github.com>
2025-08-06 11:53:59 +0800

    Update PlayerTopList.java

--- src/main/java/cn/JvavRE/playerTopList/PlayerTopList.java
+++ src/main/java/cn/JvavRE/playerTopList/PlayerTopList.java
@@ -4,9 +4,13 @@ import cn.JvavRE.playerTopList.command.Command;
 import cn.JvavRE.playerTopList.config.Config;
 import cn.JvavRE.playerTopList.data.ListsMgr;
 import cn.JvavRE.playerTopList.placeHolder.PTLExpansion;
+import net.milkbowl.vault.economy.Economy;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class PlayerTopList extends JavaPlugin {
+    private static Economy econ = null;
+
     public static PlayerTopList getInstance() {
         return getPlugin(PlayerTopList.class);
     }
@@ -15,7 +19,7 @@ public final class PlayerTopList extends JavaPlugin {
     public void onEnable() {
         // Plugin startup logic
 
-        ListsMgr.init();
+        // 必须先初始化配置，才能知道PAPI是否启用
         Config.init();
 
         new Command();
@@ -27,11 +31,41 @@ public final class PlayerTopList extends JavaPlugin {
             getLogger().warning("未检测到 PlaceholderAPI, 相关功能将无法使用");
         }
 
+        if (setupEconomy()) {
+            getLogger().info("检测到 Vault, 已成功挂钩经济功能");
+        } else {
+            getLogger().warning("未检测到 Vault 或经济插件, 财富榜将无法使用");
+        }
+
+        // 加载所有排行榜
+        ListsMgr.init();
+
+        // 首次全量更新
+        getLogger().info("正在执行首次排行榜数据更新...");
+        ListsMgr.updateAllListsOnce();
+
+        // 开启定时更新任务
         ListsMgr.startTask();
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        econ = rsp.getProvider();
+        return econ != null;
+    }
+
+    public static Economy getEconomy() {
+        return econ;
     }
 }
